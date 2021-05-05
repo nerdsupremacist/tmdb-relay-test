6634ad6f8dbb649757620e9039819d0e77abbce5
Mathias Quintero <me@quintero.io>
2021-05-05 13:30:51 +0200

    Adding suspense for loading

---
+++ src/LoadingSuspense.tsx
@@ -0,0 +1,47 @@
+
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from '@chakra-ui/react';
+import type { ReactNode } from 'react';
+import React from 'react';
+import { Suspense } from 'react';
+import type { FallbackProps } from 'react-error-boundary';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Placeholder from './Placeholder';
+
+type Props = {
+    children: ReactNode | Array<ReactNode>,
+}
+
+function ErrorFallback({ error }: FallbackProps) {
+    return (
+        <Alert
+            alignItems="center"
+            borderRadius="xl"
+            flexDirection="column"
+            justifyContent="center"
+            minH="200px"
+            padding="16px"
+            status="error"
+            textAlign="center"
+            variant="subtle"
+        >
+            <AlertIcon />
+            <AlertTitle mr={2}>An Error Occurred</AlertTitle>
+            <AlertDescription>{error.message}</AlertDescription>
+        </Alert>
+    )
+}
+
+function LoadingSuspense({ children }: Props) {
+    return (
+        <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+        >
+            <Suspense fallback={<Placeholder />}>
+                {children}
+            </Suspense>
+        </ErrorBoundary>
+    );
+}
+
+export default LoadingSuspense;
