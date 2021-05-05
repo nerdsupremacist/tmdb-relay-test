7117b61cca46bc5a8934523da60411e8b5f3c11e
Mathias Quintero <me@quintero.io>
2021-05-05 13:31:33 +0200

    Configuring linter

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  rules: {
+    quotes: [2, 'single', { avoidEscape: true }],
+    'prefer-template': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-sort-props': 'warn',
+    'react/jsx-indent-props': 'warn',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'relay/graphql-syntax': 'error',
+    'relay/compat-uses-vars': 'warn',
+    'relay/graphql-naming': 'error',
+    'relay/generated-flow-types': 'warn',
+    'relay/must-colocate-fragment-spreads': 'warn',
+    'relay/no-future-added-value': 'warn',
+    'relay/unused-fields': 'warn',
+    'relay/function-required-argument': 'warn',
+    'relay/hook-required-argument': 'warn',
+  },
+  plugins: ['simple-import-sort', 'relay', '@typescript-eslint', 'react-hooks'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:relay/recommended',
+  ],
+};
